8c93148e0c8a4a5bc80ceb533b412e5603aa98a2
Sri Phani Bhushan Mada <56060884+phani160802@users.noreply.github.com>
2025-02-22 16:31:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
 
 
-## sstreamlit APP
+## streamlit APP
 st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
 st.title("🦜 LangChain: Summarize Text From YT or Website")
 st.subheader('Summarize URL')
@@ -16,7 +16,7 @@ st.subheader('Summarize URL')
 with st.sidebar:
     groq_api_key=st.text_input("Groq API Key",value="",type="password")
 
-if not api_key:
+if not groq_api_key:
     st.info("Please add the groq api key")
 generic_url=st.text_input("URL",label_visibility="collapsed")
 
